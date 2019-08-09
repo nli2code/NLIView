2231bb800d3e5e12a8cac7971b7cee991b41163e
maxkibble <maxkibble@pku.edu.cn>
2019-08-09 13:11:28 +0800

    update content

---
+++ tipuesearch/dual/dual_content.js
@@ -0,0 +1,13 @@
+var tipuesearch = { "pages": [
+        {"title": "addProperty, Property", "text": "add some properties to docx file", "url":"exp/dual/1.html"},
+        {"title": "createPivotTable, addColumnLabel", "text": "create a pivot table", "url":"exp/dual/createPivotTable.html"},
+        {"title": "addFootnote, Document", "text": "add footnotes to a word document", "url":"exp/dual/3.html"},
+        {"title": "createCellStyle, setCellStyle", "text": "create a style with format", "url":"exp/dual/createCellStyle.html"},
+        {"title": "addImageToSheet, Sheet", "text": "add the image into the main document itself", "url":"exp/dual/addImageToSheet.html"},
+        {"title": "addStyle, setVal", "text": "add new styles to the document", "url":"exp/dual/addStyle.html"},
+        {"title": "addHyperlink, Hyperlink", "text": "create a hyperlinked cell", "url":"exp/dual/7.html"},
+        {"title": "setFillForegroundColor, setFillPattern", "text": "fill the cell with the foreground color", "url":"exp/dual/setFillForeground.html"},
+        {"title": "addNewTbl, getBody", "text": "add a table to header or footer", "url":"exp/dual/9.html"},
+        {"title": "addRelationship, create", "text": "ppt table cell using apache POI hslf/xslf", "url":"exp/dual/addRelationship.html"}
+    ]
+}
